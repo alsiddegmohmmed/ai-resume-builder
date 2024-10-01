8cdabbef9adcd5e7c8f6a3dd9b26216c045d67a1
Al Siddeg Omer <alsiddeg.omer19990@gmail.com>
2024-10-01 20:44:11 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Outlet } from 'react-router-dom';
+import './App.css'
+import { Button } from './components/ui/button';
+import './index.css';
+
+function App() {
+
+
+  return (
+    <>
+      <Outlet />
+    </>
+  )
+}
+
+export default App
